e38b8fd51cc549f1a2570c3d33acbd4fdef810da
Marak Squires <marak.squires@gmail.com>
2012-09-18 18:02:35 -0700

    [resources] Added stdin resource

---
+++ resources/stdin/index.js
@@ -0,0 +1,32 @@
+var big   = require('big'),
+    stdin = big.define('stdin');
+
+stdin.method('start', start, {
+  "description": "listens for STDIN on the process and attempts to eval it as JavaScript"
+});
+
+function start () {
+  process.listening = true;
+  process.stdin.resume();
+  process.stdin.setEncoding('utf8');
+  process.stdin.on('data', function (chunk) {
+    var lines = chunk.split('\n');
+    lines.forEach(function(line){
+      if (line) {
+        //
+        // Evaluate all STDIN in current context
+        // WARNING: This will not attempt to parse parse STDIN at all
+        // If you don't control the STDIN for this process, don't big.use('stdin').
+        //
+        try {
+          eval(line);
+        } catch (err) {
+          throw err;
+        }
+      }
+    });
+  });
+  process.stdin.on('end', function () {});
+};
+
+exports.stdin = stdin;

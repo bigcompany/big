d4940663a57c83a391826b55760701e013361f05
Marak Squires <marak.squires@gmail.com>
2012-09-19 23:05:15 -0700

    [resources] Added system resource

---
+++ resources/system/index.js
@@ -0,0 +1,28 @@
+var big = require('big'),
+    system = big.define('system');
+
+system.method('info', info);
+
+function info () {
+
+  var os  = require('os');
+
+  var obj = {};
+
+  obj.name     = "big";
+  obj.version  = "v0.0.0";
+
+  obj.system = {
+    platform: os.platform(),
+    uptime: os.uptime(),
+    loadavg: os.loadavg(),
+    totalmem: os.totalmem(),
+    cpus: os.cpus(),
+    networkInterfaces: os.networkInterfaces()
+  };
+
+  return obj;
+
+};
+
+exports.system = system;

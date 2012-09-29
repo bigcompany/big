59d991d1965b60c261b54d63aeedfb8a95ed8a02
Marak Squires <marak.squires@gmail.com>
2012-09-28 22:33:17 -0700

    [resources] Added video resource

---
+++ resources/video/index.js
@@ -0,0 +1,27 @@
+var resource = require('resource'),
+    video = resource.define('video');
+
+video.property("title", {
+  "type":"string",
+  "default": "my title",
+  "description": "the title of the video"
+});
+
+video.property("link", {
+  "type":"string",
+  "description": "the link to the video on a third party site",
+  "format": "url"
+});
+
+video.property("description", {
+  "type":"string",
+  "description": "a brief description of the video"
+});
+
+video.method("play", function () {
+}, {});
+video.method("stop", function () {
+}, {});
+
+
+exports.video = video;
